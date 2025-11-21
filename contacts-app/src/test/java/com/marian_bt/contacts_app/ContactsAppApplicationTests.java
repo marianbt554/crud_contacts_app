8defc686343fdbbdb8a59c41f1d9263cb1b5efc3
marianbt554 <botnarencomarian8@gmail.com>
2025-11-22 00:17:20 +0100

    Initial commit

---
+++ contacts-app/src/test/java/com/marian_bt/contacts_app/ContactsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marian_bt.contacts_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
