3d170913988d0da45874fc4b61bbf11d89860b4f
marianbt554 <botnarencomarian8@gmail.com>
2025-11-23 13:57:45 +0100

    Add ContactController

---
+++ contacts-app/src/main/java/com/marian_bt/contacts_app/controller/ContactController.java
@@ -0,0 +1,88 @@
+package com.marian_bt.contacts_app.controller;
+
+
+import com.marian_bt.contacts_app.domain.Contact;
+import com.marian_bt.contacts_app.service.ContactSearchCriteria;
+import com.marian_bt.contacts_app.service.ContactService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("contacts")
+
+public class ContactController  {
+
+    private final ContactService contactService;
+
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @GetMapping
+    public String ListContacts(Model model)
+    {
+        List<Contact> contacts = contactService.getAllContacts();
+        model.addAttribute("contacts", contacts);
+        model.addAttribute("contact", new ContactSearchCriteria());
+        return "contacts/list";
+    }
+
+    @GetMapping("/search")
+    public String SearchContacts(@ModelAttribute("criteria") ContactSearchCriteria criteria, Model model)
+    {
+        List<Contact> contacts = contactService.searchContacts(criteria);
+        model.addAttribute("contacts", contacts);
+        model.addAttribute("criteria", criteria);
+        return "contacts/list";
+    }
+
+    @GetMapping("/new")
+    public String ShowCreateContactForm(Model model)
+    {
+        Contact contact = new Contact();
+        model.addAttribute("contact", contact);
+        return "contacts/form";
+    }
+
+    @PostMapping
+    public String createContact(@ModelAttribute("contact") Contact contact)
+    {
+        String currentUsername = "system";
+        contactService.createContact(contact, currentUsername);
+        return "redirect:/contacts";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String ShowEditContactForm(@PathVariable("id") Long id, Model model)
+    {
+        Contact contact = contactService.getContactById(id);
+        model.addAttribute("contact", contact);
+        return "contacts/form";
+    }
+
+    @PostMapping("/{id}")
+    public String editContact(@PathVariable("id") Long id, @ModelAttribute("contact") Contact contact)
+    {
+        String currentUsername = "system";
+        contactService.updateContact(id, contact, currentUsername);
+        return "redirect:/contacts";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deleteContact (@PathVariable("id") Long id){
+        String currentUsername = "system";
+        contactService.deleteContact(id, currentUsername);
+        return "redirect:/contacts";
+    }
+
+    @GetMapping("/{id}")
+    public String viewContact(@PathVariable("id") Long id, Model model){
+        Contact contact = contactService.getContactById(id);
+        model.addAttribute("contact", contact);
+        return "contacts/detail";
+    }
+
+}
