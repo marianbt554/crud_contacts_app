1ed46da5c1f0120ae2de7b0ad41b50ad3d59687f
marianbt554 <botnarencomarian8@gmail.com>
2025-11-29 23:32:10 +0100

    Add AppUser Entity

---
+++ contacts-app/src/main/java/com/marian_bt/contacts_app/domain/AppUser.java
@@ -0,0 +1,77 @@
+package com.marian_bt.contacts_app.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "app_users")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private UserRole role =  UserRole.USER;
+
+    @Column(nullable = false)
+    private boolean enabled = true;
+
+    public AppUser() {
+
+    }
+
+    public AppUser(String username, String password, UserRole role, boolean enabled) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.enabled = enabled;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+}
