7f925a6eb955bade5448ca914d89cb0a967e3a50
marianbt554 <botnarencomarian8@gmail.com>
2025-11-29 23:34:27 +0100

    Add AppUserRepository interface

---
+++ contacts-app/src/main/java/com/marian_bt/contacts_app/repository/AppUserRepository.java
@@ -0,0 +1,11 @@
+package com.marian_bt.contacts_app.repository;
+
+import com.marian_bt.contacts_app.domain.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByUsername(String username);
+}
