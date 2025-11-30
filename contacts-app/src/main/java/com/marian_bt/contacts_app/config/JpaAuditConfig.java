24630995b3c367c550b581fd4072763fe4c5fff1
marianbt554 <botnarencomarian8@gmail.com>
2025-11-30 16:11:12 +0100

    Enable JPA auditing

---
+++ contacts-app/src/main/java/com/marian_bt/contacts_app/config/JpaAuditConfig.java
@@ -0,0 +1,18 @@
+package com.marian_bt.contacts_app.config;
+
+
+import com.marian_bt.contacts_app.security.SpringSecurityAuditorAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class JpaAuditConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return new SpringSecurityAuditorAware();
+    }
+}
---
+++ contacts-app/src/main/java/com/marian_bt/contacts_app/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,23 @@
+package com.marian_bt.contacts_app.security;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null ||
+        !authentication.isAuthenticated() ||
+        !(authentication instanceof AnonymousAuthenticationToken)){
+            return Optional.of("system");
+        }
+
+        return Optional.ofNullable(authentication.getName());
+    }
+}
