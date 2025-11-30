e4e64290365274496924fad39b51dd45f65c2163
marianbt554 <botnarencomarian8@gmail.com>
2025-11-30 15:05:34 +0100

    Add new method to AppUSerRepository interface

--- contacts-app/src/main/java/com/marian_bt/contacts_app/repository/AppUserRepository.java
+++ contacts-app/src/main/java/com/marian_bt/contacts_app/repository/AppUserRepository.java
@@ -1,6 +1,7 @@
 package com.marian_bt.contacts_app.repository;
 
 import com.marian_bt.contacts_app.domain.AppUser;
+import com.marian_bt.contacts_app.domain.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +9,6 @@ import java.util.Optional;
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 
     Optional<AppUser> findByUsername(String username);
+
+    long countByRole(UserRole role);
 }
